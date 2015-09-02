2279e71d92cec2d014bbd0cdaccd0e05db1b1f31
mazba06 <mazba09@gmail.com>
2015-09-02 18:24:56 +0600

    commit index.php

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>One Page Wonder - Start Bootstrap Template</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/one-page-wonder.css" rel="stylesheet">
+
+        <!--cdn-->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <------>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="background-color: #77CCDD;">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Money Transaction</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a href="signup.php">Sign Up</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i> <i class="glyphicon glyphicon-chevron-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="signin.php">Sign in</a></li>
+                                <li><a href="signup.php">Sign up</a></li>
+                                <!--                                <li class="divider"></li>
+                                                                <li><a href="#">About</a></li>-->
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Full Width Image Header -->
+        <header class="header-image" style="background-image: url('assets/image/banner1.jpg'); height: 400px;">
+            <div class="headline">
+                <div class="container">
+                    <!--                    <h1>One Page Wonder</h1>
+                                        <h2>Will Knock Your Socks Off</h2>-->
+
+                    <div class="row" style="margin-top: -80px;">
+                        <div class="col-lg-4 col-sm-12">
+                            <form>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                </div>                                                              
+                                <button type="submit" class="btn btn-default btn-lg btn-block btn-success">Log in</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <!-- Page Content -->
+        <div class="container">
+
+
+
+            <hr class="featurette-divider">
+
+            <!-- First Featurette -->
+            <div class="featurette" id="about">
+                <img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">
+                <h2 class="featurette-heading">Just few click to
+                    <span class="text-muted">Transfer money</span>
+                </h2>
+                <p class="lead"></p>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <!-- Second Featurette -->
+            <div class="featurette" id="services">
+<!--                <img class="featurette-image img-circle img-responsive pull-left" src="http://placehold.it/500x500">
+                <h2 class="featurette-heading">The Second Heading
+                    <span class="text-muted">Is Pretty Cool Too.</span>
+                </h2>-->
+                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+
+            <!--<hr class="featurette-divider">-->
+
+            <!-- Third Featurette -->
+            <!--            <div class="featurette" id="contact">
+                            <img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">
+                            <h2 class="featurette-heading">The Third Heading
+                                <span class="text-muted">Will Seal the Deal.</span>
+                            </h2>
+                            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                        </div>-->
+
+            <hr class="featurette-divider">
+
+            <!-- Footer -->
+            <footer class="text-center" style="color: #94cbea;">               
+                <div class="footer-above">
+                    <div class="container">
+                        <div class="row">
+                            <div class="footer-col col-lg-4">
+                                <h3>Heading</h3>
+                                <p style="color: #434343;">about | contact |</p>
+
+                            </div>
+                            <div class="footer-col col-lg-4">
+                                <h3>Around the Web</h3>
+                                <ul class="list-inline">
+                                    <li>
+                                        <a href="" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook fa-3x"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus fa-3x"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="btn-social btn-outline"><i class="fa fa-twitter fa-3x"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin fa-3x"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble fa-3x"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-col col-lg-4">
+                                <h3>Call for help</h3>
+                                <img src="assets/image/images (1).jpeg" alt="image loading problem" class="img-circle">
+                                <p style="color: #434343;">Phone: +000000-09</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-below">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12" style="color: #434343;">
+                                Site Design and develop by agency delta .Copyright &copy; agency delta 2015 all right reserve
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <!-- /.container -->
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+
+</html>
