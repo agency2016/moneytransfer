3c773b2582fc131287dfd7487fe88f005812a810
mazba06 <mazba09@gmail.com>
2015-09-03 16:42:39 +0600

    header conflict solved

--- common/header.php
+++ common/header.php
@@ -1,5 +1,5 @@
 <!-- Navigation -->
-<<<<<<< Updated upstream
+
 <?php session_start();?>
 <? //echo $_SESSION['Username'] ;?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="background-color: #77CCDD;">
@@ -55,39 +55,7 @@
 
                     ?>
                     
-=======
 
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="background-color: #77CCDD;">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Money Transaction</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="index.php">Home</a>
-                </li>
-                <li>
-                    <a href="transection.php">Transection</a>
-                </li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i> <i class="glyphicon glyphicon-chevron-down"></i></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="signin.php">Sign in</a></li>
-                        <li><a href="signup.php">Sign up</a></li>
-                        <!--                                <li class="divider"></li>
-                                                        <li><a href="#">About</a></li>-->
-                    </ul>
->>>>>>> Stashed changes
                 </li>
             </ul>
         </div>
